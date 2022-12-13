0516baa705324473952a28dd3aef0b43ae660f1b
Isaac <isaac.lin.pnc@gmail.com>
2022-12-13 16:33:37 -0500

    commit tree 456a074a1cd182d029907fcd3c1369ab2dc7021b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+
+function writeCards(names, eventName) {
+    let msgArray = [];
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        msgArray.push(message);
+    };
+    return msgArray
+};
+
+function countDown(int) {
+    for (let i = int; i >= 0; i--) {
+        console.log(i);
+    }
+}
