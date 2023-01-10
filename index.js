6afeee53a10cde043cbd21a0ad017cb5bec04d57
Isaac <isaac.lin.pnc@gmail.com>
2023-01-10 14:15:05 -0500

    finished lab.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let newArray = [];
+    for (let i = 0; i < names.length; i++) {
+        let name = names[i];
+        let msg = `Thank you, ${name}, for the wonderful ${event} gift!`
+        newArray.push(msg);
+    };
+    return newArray;
+};
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i)
+    }
+}
